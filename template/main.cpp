fe37f499c26e4dd2e30d4cacd66c853c2dce30eb
Kento Nishi <kento24gs@outlook.com>
2019-12-26 08:57:33 -0800

    Updated template

--- template/main.cpp
+++ template/main.cpp
@@ -1,12 +1,6 @@
-// Test case path: [path]
-// template - Division - Month Season
-// url
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ifstream fin("template.in");
-    ofstream fout("template.out");
     return 0;
 }
