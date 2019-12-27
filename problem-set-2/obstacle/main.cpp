0a7872157cdf0ad114137275c35a7ecc1830489c
Kento Nishi <kento24gs@outlook.com>
2019-12-27 15:57:36 -0800

    Finished obstacle

--- problem-set-2/obstacle/main.cpp
+++ problem-set-2/obstacle/main.cpp
@@ -2,24 +2,24 @@
 Obstacle Course
 ===============
 
-Consider an N x N (1 <= N <= 100) square field composed of 1
-by 1 tiles. Some of these tiles are impassible by cows and are
-marked with an 'x' in this 5 by 5 field that is challenging to
+Consider an N Row N (1 <= N <= 100) square field composed of 1
+bCol 1 tiles. Some of these tiles are impassible bCol cows and are
+marked with an 'Row' in this 5 bCol 5 field that is challenging to
 navigate:
 
-. . B x .
-. x x A .
-. . . x .
-. x . . .
-. . x . .
+. . B Row .
+. Row Row A .
+. . . Row .
+. Row . . .
+. . Row . .
 
 Bessie finds herself in one such field at location A and wants to
 move to location B in order to lick the salt block there. Slow,
 lumbering creatures like cows do not like to turn and, of course,
-may only move parallel to the edges of the square field. For
-a given field, determine the minimum number of ninety degree turns
-in any path from A to B. The path may begin and end with Bessie
-facing in any direction. Bessie knows she can get to the salt lick.
+maCol onlCol move parallel to the edges of the square field. For
+a given field, determine the minimum number of ninetCol degree turns
+in anCol path from A to B. The path maCol begin and end with Bessie
+facing in anCol direction. Bessie knows she can get to the salt lick.
 
 PROBLEM NAME: obstacle
 
@@ -28,15 +28,15 @@ INPUT FORMAT:
 * Line 1: A single integer: N
 
 * Lines 2..N + 1: Line i+1 represents row i of the field with N
-characters as above (i.e., '.', 'x', 'A', and 'B'); no spaces
+characters as above (i.e., '.', 'Row', 'A', and 'B'); no spaces
 are present on a line
 
 SAMPLE INPUT:
 
 3
-.xA
+.RowA
 ...
-Bx.
+BRow.
 
 INPUT DETAILS:
 
@@ -55,9 +55,9 @@ SAMPLE OUTPUT:
 
 OUTPUT DETAILS:
 
-The cow must make at least 2 turns: For example, the cow may start
-by facing south, move south, turn to face west, move west, move
-west, then turn to face south, and finally move south into the B
+The cow must make at least 2 turns: For eRowample, the cow maCol start
+bCol facing south, move south, turn to face west, move west, move
+west, then turn to face south, and finallCol move south into the B
 square.
 */
 
@@ -68,21 +68,21 @@ int N;
 vector<vector<bool>> grid;
 
 struct State {
-    int x;
-    int y;
+    int row;
+    int col;
     int dir;
     int dist;
 };
 
-int dy[] = {0, 1, 0, -1};
-int dx[] = {-1, 0, 1, 0};
+int dRow[] = {0, -1, 0, 1};
+int dCol[] = {1, 0, -1, 0};
 
 int main() {
     cin >> N;
-    int startX;
-    int startY;
-    int endX;
-    int endY;
+    int startRow;
+    int startCol;
+    int endRow;
+    int endCol;
     grid = vector<vector<bool>>(N, vector<bool>(N));
     for (int i = 0; i < N; i++) {
         string s;
@@ -90,60 +90,52 @@ int main() {
         for (int j = 0; j < N; j++) {
             grid[i][j] = s[j] == 'x';
             if (s[j] == 'A') {
-                startX = i;
-                startY = j;
+                startRow = i;
+                startCol = j;
             }
             if (s[j] == 'B') {
-                endX = i;
-                endY = j;
+                endRow = i;
+                endCol = j;
             }
         }
     }
     deque<State> locations;
-    vector<vector<vector<int>>> dp = vector<vector<vector<int>>>(N, vector<vector<int>>(N, vector<int>(4, -1)));
     for (int i = 0; i < 4; i++) {
-        locations.push_back({startX, startY, i, 0});
+        locations.push_back({startRow, startCol, i, 0});
     }
+    // 0 = right
+    // 1 = up
+    // 2 = left
+    // 3 = down
+    vector<vector<vector<bool>>> visited = vector<vector<vector<bool>>>(N, vector<vector<bool>>(N, vector<bool>(4, false)));
     while (locations.size() > 0) {
-        auto top = locations.front();
+        // cout << locations.size() << endl;
+        auto &top = locations.front();
         locations.pop_front();
-        if (top.x == -1 || top.x == N || top.y == -1 || top.y == N) {
+        if (top.row <= -1 || top.col <= -1 || top.row >= N || top.col >= N) {
             continue;
         }
-        if (dp[top.x][top.y][top.dir]>=0 && top.dist>dp[top.x][top.y][top.dir]) {
-            continue;
-        };
-        if (grid[top.x][top.y]) {
+        //cout << top.row << " " << top.col << " " << top.dir << endl;
+        if (visited[top.row][top.col][top.dir]) {
             continue;
         }
-        cout << "Currently at " << top.x << "," << top.y << " with distance " << top.dist << " facing direction " << top.dir << endl;
-        if(dp[top.x][top.y][top.dir]==-1){
-            dp[top.x][top.y][top.dir]=top.dist;
-        }
-        dp[top.x][top.y][top.dir] = min(top.dist,dp[top.x][top.y][top.dir]);
-        if (top.x == endX && top.y == endY) {
+        visited[top.row][top.col][top.dir] = true;
+        if (top.col == endCol && top.row == endRow) {
             cout << top.dist << endl;
             break;
-            //return 0;
         }
-        for (int i = 0; i < 4; i++) {
-            if (i == top.dir) {
-                locations.push_front({top.x + dx[i], top.y + dy[i], i, top.dist});
+        if (grid[top.row][top.col]) {
+            continue;
+        }
+        //cout << "Took " << top.dist << " to come to " << top.row << " " << top.col << endl;
+        for (int dir = 0; dir < 4; dir++) {
+            if (dir == top.dir) {
+                locations.push_front({top.row + dRow[dir], top.col + dCol[dir], dir, top.dist});
             } else {
-                int timeToTurn=(top.dir-i)==1?1:abs(top.dir-i);
-                locations.push_back({top.x, top.y, i, top.dist+timeToTurn});
+                int toTurn = abs(top.dir - dir) == 3 ? 1 : abs(top.dir - dir);
+                locations.push_back({top.row, top.col, dir, top.dist + toTurn});
             }
         }
-    }
-    for(int i=0;i<N;i++){
-        cout << "|||";
-        for(int j=0;j<N;j++){
-            for(int k=0;k<4;k++){
-                cout << dp[i][j][k] << ":";
-            }
-            cout << " ";
-        }
-        cout << endl;
     }
     return 0;
 }
