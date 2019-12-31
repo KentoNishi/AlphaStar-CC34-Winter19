3078dd5179a147fb024802470564b9eaa63e97b1
Kento Nishi <kento24gs@outlook.com>
2019-12-31 10:29:59 -0800

    Started problem set 6

---
+++ problem-set-6/guardian/main.cpp
@@ -0,0 +1,62 @@
+/*
+Guarding the Farm
+=================
+
+The farm has many hills upon which Farmer John would like to place
+guards to ensure the safety of his valuable milk-cows.
+
+He wonders how many guards he will need if he wishes to put one on
+top of each hill. He has a map supplied as a matrix of integers;
+the matrix has N (1 < N <= 700) rows and M (1 < M <= 700) columns.
+Each member of the matrix is an altitude H_ij (0 <= H_ij <= 10,000).
+Help him determine the number of hilltops on the map.
+
+A hilltop is one or more adjacent matrix elements of the same value
+surrounded exclusively by either the edge of the map or elements
+with a lower (smaller) altitude. Two different elements are adjacent
+if the magnitude of difference in their X coordinates is no greater
+than 1 and the magnitude of differences in their Y coordinates is
+also no greater than 1.
+
+PROBLEM NAME: guardian
+
+INPUT FORMAT:
+
+* Line 1: Two space-separated integers: N and M
+
+* Lines 2..N+1: Line i+1 describes row i of the matrix with M
+        space-separated integers: H_ij
+
+SAMPLE INPUT:
+
+8 7
+4 3 2 2 1 0 1
+3 3 3 2 1 0 1
+2 2 2 2 1 0 0
+2 1 1 1 1 0 0
+1 1 0 0 0 1 0
+0 0 0 1 1 1 0
+0 1 2 2 1 1 0
+0 1 1 1 2 1 0
+
+OUTPUT FORMAT:
+
+* Line 1: A single integer that specifies the number of hilltops
+
+SAMPLE OUTPUT:
+
+3
+
+OUTPUT DETAILS:
+
+There are three peaks: The one with height 4 on the left top, one of
+the points with height 2 at the bottom part, and one of the points with
+height 1 on the right top corner.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    return 0;
+}
